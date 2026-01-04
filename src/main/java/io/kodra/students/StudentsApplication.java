b459cec4c3a59b4f30b219fc77d71d6ecf0b0b19
oalam <omar.alamad@devophubjo.com>
2026-01-04 14:12:21 +0300

    make app to log hello from jenkins every 1 sec

--- src/main/java/io/kodra/students/StudentsApplication.java
+++ src/main/java/io/kodra/students/StudentsApplication.java
@@ -6,8 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StudentsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsApplication.class, args);
 
+        // Print every second
+        while (true) {
+            System.out.println("Hello from Jenkins Pipeline");
+            try {
+                Thread.sleep(1000); // 1 second
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
 }
